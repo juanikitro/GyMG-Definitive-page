5533f76e97b67b096d6ea687b7d8427b447bc761
Juani Kitroser <jpkitro@gmail.com>
2021-06-20 19:11:21 -0300

    fix bugs

--- scrollrevealscript.js
+++ scrollrevealscript.js
@@ -1,18 +1,18 @@
 window.sr = ScrollReveal();
 sr.reveal('.scrollToTop', {
-	duration: 1500,
+	duration: 1000,
 	origin: 'top',
 	distance: '-50px',
 	easing: 'ease-in-out',
 });
 sr.reveal('.scrollToLeft', {
-	duration: 1500,
+	duration: 1000,
 	origin: 'left',
 	distance: '100px',
 	easing: 'ease-in-out',
 });
 sr.reveal('.scrollToRight', {
-	duration: 1500,
+	duration: 1000,
 	origin: 'right',
 	distance: '100px',
 	easing: 'ease-in-out',
