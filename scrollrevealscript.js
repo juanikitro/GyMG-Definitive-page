2c62d63ac7497e4220ec50216630dd045f90a289
Juani Kitroser <jpkitro@gmail.com>
2021-06-20 18:04:41 -0300

    animation add

---
+++ scrollrevealscript.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal();
+sr.reveal('.scrollToTop', {
+	duration: 1500,
+	origin: 'top',
+	distance: '-50px',
+	easing: 'ease-in-out',
+});
+sr.reveal('.scrollToLeft', {
+	duration: 1500,
+	origin: 'left',
+	distance: '100px',
+	easing: 'ease-in-out',
+});
+sr.reveal('.scrollToRight', {
+	duration: 1500,
+	origin: 'right',
+	distance: '100px',
+	easing: 'ease-in-out',
+});
